e823e7c658feb06777fb627c2c726ee1f29a4088
Maria <146016427+mgfuentes-ct@users.noreply.github.com>
2025-09-12 10:02:57 -0600

    Primer commit

---
+++ backend/database.py
@@ -0,0 +1,21 @@
+# backend/database.py
+import mysql.connector
+from mysql.connector import Error
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+        if connection.is_connected():
+            return connection
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        return None
